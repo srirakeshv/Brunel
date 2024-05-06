009ebf7fab5c73e426ae0ed6ca48c6cf3473fc8f
srirakeshv <srirakeshv@gmail.com>
2024-05-06 20:16:54 +0530

    uploaded files and updated homepage

---
+++ src/Pages/HomePage/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../../Components/Common/Navbar";
+import Banner from "../../Components/Home/Banner";
+import Accordin from "../../Components/Home/Accordin";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+      <Accordin />
+    </div>
+  );
+};
+
+export default Homepage;
